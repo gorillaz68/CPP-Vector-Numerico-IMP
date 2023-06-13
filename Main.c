5c2b44c14916b14b74fdaa390693681a6e39240c
Luis Alfredo Villamizar Lopez <136473748+gorillaz68@users.noreply.github.com>
2023-06-13 19:00:36 -0400

    Update and rename Main.c to Main.cpp

--- Main.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include "admin.h"
-int main(void){
-    
-    admin();
-    return 0;
-}
---
+++ Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+#include "admin.h"
+
+int main(void){
+    // Your code here!
+    admin();
+    return 0;
+}
+
