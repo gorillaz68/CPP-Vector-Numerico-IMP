c76f3d3d893de39377b7f8c1427114d983333e57
Luis Alfredo Villamizar Lopez <136473748+gorillaz68@users.noreply.github.com>
2023-06-13 19:11:44 -0400

    Create salida.h
    Módulo funcional Límite tipo Borde

---
+++ salida.h
@@ -0,0 +1,15 @@
+void escribir_num(int num[],int i){
+    cout<<"el numero "<<i+1<<" es:"<<num[i]<<endl;
+}
+void escribir(int ac){
+    cout<<"el resultado es:"<<ac<<endl;
+}
+void escribir_tam(int tam){
+     cout<<"Ingrese la cantidad de numeros a operar:"<<tam<<endl;
+}
+void escribir_exc(){
+    cout<<"ingrese un valor válido"<<endl;
+}
+void escribir_salir(){
+    cout<<"Hasta la Vista"<<endl;
+}
