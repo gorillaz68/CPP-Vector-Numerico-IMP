b21dda702b1e47a4524f1792b2a2fdea852e0335
Luis Alfredo Villamizar Lopez <136473748+gorillaz68@users.noreply.github.com>
2023-06-13 19:12:41 -0400

    Create menu.h
    Modulo funcional Límite tipo Interfaz

---
+++ menu.h
@@ -0,0 +1,13 @@
+int menu(){
+    int opc;
+    cout<<"***menu del programa***\n";
+    cout<<"1.-Sumar\n";
+    cout<<"2.-Restar\n";
+    cout<<"3.-Multiplicar\n";
+    cout<<"4.-Dividir\n";
+    cout<<"3.-salir\n";
+    cin>>opc;
+    cout<<"Ingrese la opción: "<<opc<<endl;
+    return opc;
+    
+}
